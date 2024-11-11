52b44e738d94c33175263cb43ec6653aaf60944f
alvaro <alvarolespio@estudiante.mdp.utn.edu.ar>
2024-11-10 23:30:38 -0300

    error en la urlUser solucionado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   tmdbApiKey: 'ee6b4c947f589b3e4d73feedd760f254',  // clave de api
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
-  urlUser: '"http://localhost:3000/usuarios"'
+  urlUser: 'http://localhost:3000/usuarios'
 };
