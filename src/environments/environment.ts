f5ca6b0c5f0b46d52e320c9a131a5db088eb6ba7
ivanamaya1 <ivancitohh2004@hotmail.com>
2024-11-10 22:59:26 -0300

    agregue url de usuarios a environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   tmdbApiKey: 'ee6b4c947f589b3e4d73feedd760f254',  // clave de api
-  tmdbBaseUrl: 'https://api.themoviedb.org/3'
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  urlUser: '"http://localhost:3000/usuarios"'
 };
